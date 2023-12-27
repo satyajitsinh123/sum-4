9e59c7fa6013712b98e8084243c8bd42eabfcf8b
satyajitsinh123 <satyajitsinh63@gamil.com>
2023-12-27 11:57:50 +0530

    hello

---
+++ index.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    float F = ( 38 * 9/5 ) + 32;
+printf("%f",F);
+
+
+ int BS=100;
+    int HAR=10;
+    int DA=5;
+    int TA=8;
+    printf("%d",BS+HAR+DA+TA);
+
+ int fa=65;
+    int sa=45;
+    int ta=fa+sa;
+    printf("%d",180-ta);
+
+  return 0;
+}
